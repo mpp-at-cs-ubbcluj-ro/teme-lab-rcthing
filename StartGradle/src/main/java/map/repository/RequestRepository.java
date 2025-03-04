bdc8e86ea67ad5d3ff76cbd4f8e43a7af52c8ca7
rcthing <rednicluca@gmail.com>
2025-03-04 10:51:23 +0200

    tot ce am facut in laboratorul 2

---
+++ StartGradle/src/main/java/map/repository/RequestRepository.java
@@ -0,0 +1,7 @@
+package map.repository;
+
+import map.model.ComputerRepairRequest;
+
+public class RequestRepository extends AbstractRepository<ComputerRepairRequest, Integer>{
+    public RequestRepository(){}
+}
